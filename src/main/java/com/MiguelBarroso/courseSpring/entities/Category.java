bc80f34766cd18869e41b7d054dd7e93e1de89cc
Miguel Barroso <miguelreisbarroso@gmail.com>
2025-07-24 12:22:50 +0100

    Establish many-to-many relationship between Product and Category entities

--- src/main/java/com/MiguelBarroso/courseSpring/entities/Category.java
+++ src/main/java/com/MiguelBarroso/courseSpring/entities/Category.java
@@ -1,5 +1,6 @@
 package com.MiguelBarroso.courseSpring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -14,11 +15,14 @@ public class Category implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
